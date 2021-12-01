5e5a07986e409ff487bdc86c2e4937555d4993d2
mike whitfield <mwhitfieldwork@gmail.com>
2021-11-30 20:42:37 -0600

    Added the products controller to the solution

---
+++ NorthwindApiWithId/NorthwindApiWithId/Models/ProductDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace NorthwindApiWithId.Models
+{
+    [DataContract(Name = "Product")]
+    [Serializable]
+    public class ProductDTO
+    {
+        [DataMember]
+        public int ProductID { get; set; }
+        [DataMember]
+        public string ProductName { get; set; }
+        [DataMember]
+        public Nullable<int> SupplierID { get; set; }
+        [DataMember]
+        public Nullable<int> CategoryID { get; set; }
+        [DataMember]
+        public string QuantityPerUnit { get; set; }
+        [DataMember]
+        public Nullable<decimal> UnitPrice { get; set; }
+        [DataMember]
+        public Nullable<short> UnitsInStock { get; set; }
+        [DataMember]
+        public Nullable<short> UnitsOnOrder { get; set; }
+        [DataMember]
+        public Nullable<short> ReorderLevel { get; set; }
+        [DataMember]
+        public bool Discontinued { get; set; }
+    }
+}
---
+++ NorthwindApiWithId/NorthwindApiWithId/Models/ProductMoreDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace NorthwindApiWithId.Models
+{
+    [DataContract(Name = "ProductMore")]
+    [Serializable]
+    public class ProductMoreDTO
+    {
+        [DataMember]
+        public int ProductID { get; set; }
+        [DataMember]
+        public string ProductName { get; set; }
+        [DataMember]
+        public int SupplierID { get; set; }
+        [DataMember]
+        public string CompanyName { get; set; }
+        [DataMember]
+        public string ContactName { get; set; }
+    }
+}
